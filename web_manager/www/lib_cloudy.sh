5964b2ed83c4b69be44e601eb7b81a634fdc068c
gaotan <cloudy@deby.(none)>
2012-02-22 06:44:39 -0500

    cloudy

--- web_manager/www/lib_cloudy.sh
+++ web_manager/www/lib_cloudy.sh
@@ -1,4 +1,35 @@
 
+cloudy_task()
+{
+    task_name="$1"
+    shift
+    cloudy_tasks_path="/tmp/cloudy/tasks"
+
+    touch "/tmp/wsh/$PPID/forked"
+
+    (
+	task_pid="$(env | grep '^_=' | cut -d '*' -f 2)"
+	task_path="$cloudy_tasks_path/$task_pid"
+	
+	rm -f "$task_path/"*
+	mkdir -p "$task_path"
+	date "+%s" > "$task_path/begin"
+	echo "$task_name" > "$task_path/name"
+	
+	cmd=("$1")          #
+	shift               #
+	while [ $# -gt 0 ]  # 
+	do                  # "${cmd[@]}" is the same thing than "$*"
+	    cmd+=("$1")     # But handle 'spaces' in arguments
+	    shift           #
+	done                #
+	("${cmd[@]}") 2>> "$task_path/log" >> "$task_path/log"
+	
+	echo $? > "$task_path/exit_code"
+	date "+%s" > "$task_path/end"
+    ) &
+}
+
 cloudy_scenario_count()
 {
     ls "$1/play()" | wc -l
@@ -26,6 +57,7 @@ cloudy_reachable_machines()
 
 cloudy_copy_scenario_and_replace_variables()
 {
+    (
     src_path="$1"
     dst_path="$2"
     tmp_path="/tmp/.cloudy_copy_scenario_and_replace_variables_$$"
@@ -35,17 +67,14 @@ cloudy_copy_scenario_and_replace_variables()
     rmdir "$dst_path"
     cp -RL "$src_path" "$tmp_path"
     replace_command="sed"
-    set | grep PARAM_var_ | while read -r kv
+    wsh_input_list | grep "^var_" | sed "s/^var_//" | while read -r input
     do
-	key="$(echo "$kv" | cut -d '=' -f 1 | sed s/^PARAM_var_//)"
-
-	val="$(echo "$kv" | sed s/'^[^=]*='//)"
+        val="$(wsh_input "var_$input")"
 	hexa_val=$(echo -n "$val" | sed 's/\\/\\\\/g' | hexdump -v -e '"\\\x" 1/1 "%02x" ' | sed s/'\\x26'/'\\\&'/g)
-	echo "  [$key]  :  [$val]  ($hexa_val)"
-	replace_command="$replace_command -e s/<CLOUDY:$key>/$hexa_val/g"
+	replace_command="$replace_command -e s/<CLOUDY:$input>/$hexa_val/g"
+	echo "$input=$val" >> "$tmp_path/variables.ini"
     done
 
-    echo "$replace_command"
     find "$tmp_path" | grep '<CLOUDY:.*>' | while read -r path
     do
 	new_path="$(echo "$path" | $replace_command)"
@@ -57,11 +86,12 @@ cloudy_copy_scenario_and_replace_variables()
 	mv  "$path.cloudy_swap_$$" "$path"
     done
     mv "$tmp_path" "$dst_path"
+    )
 }
 
 cloudy_search_scenario_variables()
 {
-    grep -roh '<CLOUDY:.*>' "$1" | sort | uniq | sed 's/<CLOUDY:\(.*\)>/\1/g'
+    grep -roh '<CLOUDY:[^>]*>' "$1" | sort | uniq | sed 's/<CLOUDY:\(.*\)>/\1/g'
 }
 
 cloudy_list_default_scenario_variables()
@@ -82,17 +112,24 @@ cloudy_list_default_scenario_variables()
 	cur_path="$cur_path/$path"
 	cp -P "$cur_path/variables()/"* "$tmp_var_path/."
     done
-
+    
     find "$tmp_var_path" -type f | while read var_path
     do
 	var_name="$(basename "$var_path")"
 	if [[ -x "$var_path" ]]
 	then
-	    var_delault_val="$("$var_path" | sed -e 's/^/"/g' -e 's/$/"/g' | tr '\n' ',' | sed 's/,$//g')"
+            if [[ -n "$(echo "$var_name" | grep -E '^.*\..*$')" ]]
+            then
+	        var_default_val="$("$var_path")"
+	        echo "\"default_$var_name\": $var_default_val,"
+            else
+	        var_default_val="$("$var_path" | sed -e 's/^/"/g' -e 's/$/"/g' | tr '\n' ',' | sed 's/,$//g')"
+	        echo "\"default_$var_name\": [$var_default_val],"
+            fi
 	else
-	    var_delault_val="$(cat "$var_path" | sed -e 's/^/"/g' -e 's/$/"/g' | tr '\n' ',' | sed 's/,$//g')"
+	    var_default_val="$(cat "$var_path" | sed -e 's/^/"/g' -e 's/$/"/g' | tr '\n' ',' | sed 's/,$//g')"
+	    echo "\"default_$var_name\": [$var_default_val],"
 	fi
-	echo "\"default_$var_name\": [$var_delault_val],"
     done
     rm -rf "$tmp_var_path"
 }
